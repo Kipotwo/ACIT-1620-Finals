a2a4c4e531b71e1f46245bd6a0fd7b3432f3f20a
Aron Yang <ayang100@my.bcit.ca>
2020-12-09 12:40:00 -0800

    Removed Images, Added Function to add images

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
 let footer = document.querySelector('Footer')
 footer.style.display = 'none';
 
-let images = document.querySelectorAll('img') 
+for (let num = 0; num < 5; num++){
+    let div = document.querySelector('div')
+    let newImage = document.createElement('img')
+    newImage.setAttribute('src', 'images/pic' + (num+1) + '.jpg')
+    newImage.setAttribute('alt', 'pic' + (num+1) + '.jpg')
+    div.append(newImage)
+}
 
 function hoverImage(){
     let displayImage = document.querySelector('.displayed-img')
     displayImage.setAttribute('src') = ''
     displayImage.setAttribute('alt') = ''
 }
-
-images.addEventListener('mouseover', hoverImage)
