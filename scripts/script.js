a1085fc6ad09f5b4acbc2669fac2744ddce686ca
Aron Yang <ayang100@my.bcit.ca>
2020-12-09 13:31:57 -0800

    Tweaked Hover Event to work with new code

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,25 @@ for (let num = 0; num < 5; num++){
     newImage.setAttribute('src', 'images/pic' + (num+1) + '.jpg')
     newImage.setAttribute('alt', 'pic' + (num+1) + '.jpg')
     div.append(newImage)
+    newImage.addEventListener('mouseover', hoverImage)
 }
 
-function hoverImage(){
+function hoverImage(event){
+    console.log(event.target)
     let displayImage = document.querySelector('.displayed-img')
-    displayImage.setAttribute('src') = ''
-    displayImage.setAttribute('alt') = ''
+    let newsrc = event.target.getAttribute('src')
+    let newalt = event.target.getAttribute('alt')
+    displayImage.setAttribute('src', newsrc)
+    displayImage.setAttribute('alt', newalt)
+    footer.style.display= 'initial'
 }
+
+function clearInput(){
+    input.value = '';
+}
+
+let input = document.querySelector('input')
+
+let button = document.querySelector('button')
+
+button.addEventListener('click', clearInput)
